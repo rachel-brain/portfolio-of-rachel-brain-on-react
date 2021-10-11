1caf85052d2e2add78a08472f03590a30abad0ed
Rachel Brain <rachel.brain@internode.on.net>
2021-10-11 20:08:20 +1100

    3rd commit with corrected package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+// import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+// const App = () => <PortfolioContainer />;
+
+const App = () => <h1>Rachel</h1>
 
 export default App;
